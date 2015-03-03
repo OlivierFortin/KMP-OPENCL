71bf45fc8e494db8f87466750fd8bb2cc2d080cc
Olivier Fortin <olivierf34@gmail.com>
2015-03-03 14:02:29 -0500

    Fonctionnal algo

--- run.py
+++ run.py
@@ -7,6 +7,28 @@ import pyopencl as cl
 import csv
 import pyopencl.array as pycl_array 
 
+# from wikipedia algo
+# Modify to use np array
+def partial(W):
+	
+	T = np.random.rand(len(W)).astype(np.int32)
+	T[0] = -1
+	T[1] = 0
+	pos = 2
+	cnd = 0
+
+	while (pos < len(W)):
+		if W[pos-1] == W[cnd]:
+			cnd = cnd+1
+			T[pos] = cnd
+			pos = pos +1
+		elif cnd > 0:
+			cnd = T[cnd]
+		else:
+			T[pos] = 0
+			pos = pos +1
+	return T
+
 a_np = np.random.rand(50).astype(np.float32)
 b_np = np.random.rand(50).astype(np.float32)
 
@@ -15,16 +37,20 @@ opencl_apps = open('opencl/basicTest.cl', 'r').read()
 # load all csv file
 # input_csv
 
-
-
-
 input_csv = genfromtxt('Data/Data2/inputFile.csv', delimiter=',').astype(np.int32)
 states_automate_csv = genfromtxt('Data/Data2/Automate.csv', delimiter=',').astype(np.int32)
-# alphabets_csv = genfromtxt('Data/Data1/ListAlphabet.csv', delimiter='\n').astype(np.str_)
-# output_states_csv = genfromtxt('Data/Data1/OutputStates.csv', delimiter='\n').astype(np.int32)
-# states_tab_csv = genfromtxt('Data/Data1/StatesTab.csv', delimiter=';').astype(np.int32)
-# print states_tab_csv
+test  = genfromtxt('Data/Data2/Automate.csv', delimiter=',')
 
+failureTable  = []
+with open('Data/Data2/Automate.csv', 'rb') as csvfile: 
+	automate = csv.reader(csvfile, delimiter=',')
+	automate_str = ""
+	for row in automate:
+		automate_str = automate_str.join(row)
+	failureTable = partial(automate_str)
+
+print failureTable
+print 
 # Create Context and queue
 ctx = cl.create_some_context();
 queue = cl.CommandQueue(ctx)
@@ -32,10 +58,13 @@ queue = cl.CommandQueue(ctx)
 mf = cl.mem_flags
 res_input = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=input_csv)
 res_automate = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=states_automate_csv)
+res_failure_table = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=failureTable)
 
+
+nb_process = 16
 
 prg = cl.Program(ctx, opencl_apps).build()
-prg.CheckMatch(queue, (16,), None, np.int32(72), np.int32(128), res_input, res_states_tab)
+prg.find_pattern(queue, (nb_process,), None, np.int32(72), np.int32(len(input_csv)/nb_process),np.int32(len(input_csv)),np.int32(len(states_automate_csv)), res_input, res_automate, res_failure_table)
 
 
 
